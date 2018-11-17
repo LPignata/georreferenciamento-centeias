7df3a2886403ad4326fab6cb660174010dd00322
LPignata <luanpignata@aluno.unb.br>
2018-11-17 18:33:43 -0200

    Adicionada a barra de pesquisa

--- static/script.js
+++ static/script.js
@@ -21,7 +21,6 @@ function style(color) {
 }
 
 function fill_estates_map(data) {
-    console.log(data);
     data.countries.forEach(element => {
         countrie = element.countrie;
         coordinates = element.coordinates;
@@ -47,4 +46,69 @@ $(document).ready(function() {
     $.getJSON("static/countrie.json").done(function(data) {
         fill_estates_map(data);
     });
+
+    // Make the DIV element draggable:
+    dragElement(document.getElementById("search"));
+
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "-title")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById(elmnt.id + "-title").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV: 
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            if ((elmnt.offsetTop - pos2) >= 0) {
+                if ((elmnt.offsetTop - pos2 + $("#search").height()) <= $(document).height()) {
+                    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                }
+                else {
+                    elmnt.style.top = ($(document).height() - $("#search").height()) + "px";
+                }
+            }
+            else {
+                elmnt.style.top = "0px";
+            }
+
+            if ((elmnt.offsetLeft - pos1) >= 0) {
+                if ((elmnt.offsetLeft - pos1 + $("#search").width()) <= $(document).width()) {
+                    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+                }
+                else {
+                    elmnt.style.left = ($(document).width() - $("#search").width()) + "px";
+                }
+            }
+            else {
+                elmnt.style.left = "0px";
+            }
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
 });
