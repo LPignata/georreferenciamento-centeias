177b84f4a44a30236e75d0d4681030c7dd1a05ee
leooleo <leo.ms097@gmail.com>
2018-12-06 19:14:07 -0200

    Adicionadas linhas para debug

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ logging.basicConfig(filename='static/entries.log', level=logging.ERROR)
 app = Flask(__name__, static_url_path='/static')
 
 database_url = "https://news-banco.centeias.net/retrieve?"
+# database_url = 'https://sala-de-situacao-bd.herokuapp.com/retrieve?'
 
 # parametros de filtragem
 params_dict = {'disease': '', 'globe': '', 'data_begin': '', 'data_end': ''}
@@ -20,6 +21,7 @@ params_dict = {'disease': '', 'globe': '', 'data_begin': '', 'data_end': ''}
 @app.route("/access/dates")
 # Retorna quantos acessos cada data teve
 def get_dates():
+    print('asking for number of access by date')
     with open('static/entries.log') as my_file:
         lines = my_file.readlines()
 
@@ -38,6 +40,7 @@ def get_dates():
 @app.route("/access/ips")
 # Retorna quantas vezes cada ip acessou
 def get_ips():
+    print('asking for number of access by ips')
     with open('static/entries.log') as my_file:
         lines = my_file.readlines()
 
@@ -70,6 +73,7 @@ def get_database_diseases():
 
 @app.route('/get_database_search')
 def get_database_search():
+    print('A search has been requested')
     # Prepara a query a ser enviada para o banco de dados
     global params_dict
     search_query = ''
@@ -98,40 +102,50 @@ def get_database_search():
     data = retrieve_json(database_url + search_query)
     
     if data == None:
+        print('The search parameters returned nothing')
         return jsonify({})
 
     if(params_dict['disease'] == ''):
         params_dict['disease'] == 'Todas doenças'
-
-
-    print(jsonify(process_json(data, params_dict['globe'] == "countries", date, params_dict['disease'])))
+    
+    print('the search requisition is completed')
     return jsonify(process_json(data, params_dict['globe'] == "countries", date, params_dict['disease']))
 
 # Testa se o banco está disponível
 def database_availability():
+    print('Testing database availability...')
     request = requests.get(database_url, verify=False)
+    print('Database returned http code: ' + str(request.status_code))
     return request.status_code == 200
 
 @app.route('/')
 def main_page(name=None):
+    print('Home page request')
     # Caso o banco não esteja disponível alerte o usuário
     if(database_availability() == False):
+        print('Database didn\'t return http 200!')
         return render_template('db_error.html')
+    
+    print('Database functioning correctly!')
 
     global params_dict
     logger = logging.getLogger()
     date = datetime.date.today()
     ip   = request.environ['REMOTE_ADDR']
 
+    print('logged ip')
+
     logger.error(str(ip) + ' ' + str(date))
     
     params_dict['disease'] = request.args.get('disease','')
     params_dict['globe'] = request.args.get('globe','')
     params_dict['data_begin'] = request.args.get('data_begin','')    
-    params_dict['data_end'] = request.args.get('data_end','')    
+    params_dict['data_end'] = request.args.get('data_end','')        
 
+    print('Returning home page html')
     return render_template('index.html')
 
 if __name__ == "__main__":    
+    print('Starting app...')
     # app.run(port=3000, debug=True)    
     serve(app, port=5000)
